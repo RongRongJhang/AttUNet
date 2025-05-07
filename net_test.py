6a82d2daa9f7b6af5473df9281583546428a9259
RongRongJhang <dandelionron@gmail.com>
2025-05-07 10:19:47 +0800

    "fix net_test"

--- net_test.py
+++ net_test.py
@@ -1,7 +1,7 @@
 from thop import profile
 import torch
 import time
-from model_test import LYT
+from model import LYT
 
 # 設定裝置
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
